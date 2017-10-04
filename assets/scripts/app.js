960663f102ffe559a753993cc0b154b7ea26b7c8
NrupM <maryenorthrup@gmail.com>
2017-10-04 14:32:01 -0700

    update outfitter description

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    // Smooth scrolling when nav and btn link clicked
     $('.scroll-nav').click(function (e) {
         const linkHref = $(this).attr('href');
         $('html, body').animate({
@@ -6,4 +7,18 @@ $(document).ready(function () {
         }, 1000);
         e.preventDefault()
     })
+
+    // Smooth scrolling using jQuery easing
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: (target.offset().top - 48)
+                }, 1000, "easeInOutExpo");
+                return false;
+            }
+        }
+    });
 })
